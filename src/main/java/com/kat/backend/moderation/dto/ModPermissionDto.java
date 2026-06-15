e1a49899978f313a0345d6a1503113971f1fd41e
rondrft <rondrft@gmail.com>
2026-06-14 21:48:29 -0300

    feat: add all mod permission fields to ModPermissionDto

--- src/main/java/com/kat/backend/moderation/dto/ModPermissionDto.java
+++ src/main/java/com/kat/backend/moderation/dto/ModPermissionDto.java
@@ -7,8 +7,20 @@ import java.util.List;
 public class ModPermissionDto {
     private List<String> xkick;
     private List<String> xban;
+    private List<String> xsoftban;
+    private List<String> xtempban;
+    private List<String> xunban;
     private List<String> xmute;
     private List<String> xunmute;
     private List<String> xwarn;
     private List<String> xhistory;
+    private List<String> xwarnings;
+    private List<String> xclearwarns;
+    private List<String> xnuke;
+    private List<String> xslowmode;
+    private List<String> xlock;
+    private List<String> xunlock;
+    private List<String> xfilter;
+    private List<String> xwhitelist;
+    private List<String> xmodconfig;
 }
